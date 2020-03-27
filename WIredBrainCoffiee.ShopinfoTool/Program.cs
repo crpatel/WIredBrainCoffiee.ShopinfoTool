a60588027477efebaaaa088bea08766e4a6d68bc
Chirag Patel <Chirag Patel@DESKTOP-2LBT7HM>
2020-03-27 14:46:43 +0530

    Implemented 'help' command in main project

--- WIredBrainCoffiee.ShopinfoTool/Program.cs
+++ WIredBrainCoffiee.ShopinfoTool/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using WiredBrainCoffee.DataAccess;
 
 namespace WIredBrainCoffiee.ShopinfoTool
 {
@@ -6,7 +7,23 @@ namespace WIredBrainCoffiee.ShopinfoTool
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Wired Brain Coffee Shop Information Tool...");
+            Console.WriteLine("Write 'help' to list all commands");
+            var coffeeShopDataProvider = new CoffeeShopDataProvider();
+
+            while (true)
+            {
+                var line = Console.ReadLine();
+                var coffeeShops = coffeeShopDataProvider.LoadCoffeeShops();
+                if(string.Equals("help",line,StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("> Available Coffee Shop commands:");
+                    foreach (var coffeeShop in coffeeShops)
+                    {
+                        Console.WriteLine($"> {coffeeShop.Location}");
+                    }
+                }
+            }
         }
     }
 }
