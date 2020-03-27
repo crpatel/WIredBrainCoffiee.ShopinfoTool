c7675a8f3faf425c6d8ac8d1f8a0256ab287c8c4
Chirag Patel <Chirag Patel@DESKTOP-2LBT7HM>
2020-03-27 14:44:53 +0530

    Added Data Access Project to solution

---
+++ WiredBrainCoffee.DataAccess/CoffeeShopDataProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WiredBrainCoffee.DataAccess.Model;
+
+namespace WiredBrainCoffee.DataAccess
+{
+    public class CoffeeShopDataProvider
+    {
+        public IEnumerable<CoffeeShop> LoadCoffeeShops()
+        {
+            yield return new CoffeeShop { Location = "Rajkot", BeansInStockInKg = 900 };
+            yield return new CoffeeShop { Location = "Ahmedabad", BeansInStockInKg = 90 };
+            yield return new CoffeeShop { Location = "Surat", BeansInStockInKg = 300 };
+        }
+    }
+}
