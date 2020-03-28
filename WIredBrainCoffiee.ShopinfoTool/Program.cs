242715b07f907d53bbbbd0006ad607dd62e1b9f5
Chirag Patel <chiragrpatel@live.com>
2020-03-28 15:06:32 +0530

    Added Handle Command - refactoring

--- WIredBrainCoffiee.ShopinfoTool/Program.cs
+++ WIredBrainCoffiee.ShopinfoTool/Program.cs
@@ -30,26 +30,8 @@ namespace WIredBrainCoffiee.ShopinfoTool
                 }
                 else
                 {
-                    var foundCoffeeShops = coffeeShops.Where(x => x.Location.StartsWith(line, StringComparison.OrdinalIgnoreCase)).ToList();
-                    if(foundCoffeeShops.Count == 0)
-                    {
-                        Console.WriteLine($"> Command '{line}' not found");
-                    }
-                    else if(foundCoffeeShops.Count == 1)
-                    {
-                        var coffeeShop = foundCoffeeShops.Single();
-                        Console.WriteLine($"Location: {coffeeShop.Location}");
-                        Console.WriteLine($"Beans in Stock: {coffeeShop.BeansInStockInKg} kg");
-                        Console.WriteLine($"Paper Cups in Stock {coffeeShop.PaperCup}");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Multiple matching coffeeshops found");
-                        foreach (var item in foundCoffeeShops)
-                        {
-                            Console.WriteLine($"> {item.Location}");
-                        }
-                    }
+                    var commandHandler = new CoffeeShopCommandHandler(coffeeShops, line);
+                    commandHandler.HandleCommand();
                 }
             }
         }
